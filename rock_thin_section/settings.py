b1a5d0ce8dce6b9c01b2228d211eb87017c8fca8
BackNumberyyds <backnumberrrr@gmail.com>
2023-02-11 17:25:48 +0800

    basic photo viewer

--- rock_thin_section/settings.py
+++ rock_thin_section/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+    ('node_modules', os.path.join(BASE_DIR, 'node_modules/')),
+)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
