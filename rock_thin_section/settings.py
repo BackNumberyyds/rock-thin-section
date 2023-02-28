8b387febb6bef79507be34f00c086678a51a309f
BackNumberyyds <backnumberrrr@gmail.com>
2023-02-28 09:39:38 +0800

    startapp frontend

--- rock_thin_section/settings.py
+++ rock_thin_section/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rock.apps.RockConfig'
+    'rock.apps.RockConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
