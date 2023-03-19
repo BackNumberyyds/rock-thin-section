a6a7648d0d4b7c720c341efe31b38055dd832011
BackNumberyyds <backnumberrrr@gmail.com>
2023-03-19 16:34:56 +0800

    Adjust react components paths

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Widget from './checkbox-tree';
-import SearchForm from './allphoto-search-form';
+import ReactDOM from 'react-dom/client';
+import SearchForm from './allphoto-search-form/allphoto-search-form';
 
-ReactDOM.render(<SearchForm />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<SearchForm />);
